cf4f03d34b3806008f7ab91a1f49579ca8b4c37c
PaulandrewIgloso <iglosopaul310@gmail.com>
2026-01-09 04:39:59 +0800

    feat: add Product interface and productService for managing product data

---
+++ src/services/productService.ts
@@ -0,0 +1,2 @@
+import axios from "axios";
+import type { Product } from "@/types/selling.types";
---
+++ src/types/product.types.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    name: string;
+    price: number;
+    status: number;
+    brand: string;
+    description?: string;
+    imageUrl?: string;
+    weight?: number;
+    width?: number;
+    height?: number;
+    length?: number;
+}
