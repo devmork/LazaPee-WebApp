ea211482b123dfa34f9d43765eeb5f79ce5f08e0
devmork <enoymarkden@gmail.com>
2026-01-09 01:54:00 +0800

    feat: implement routing for LogIn, SignUp, and ProductList components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
+import Layout from "./components/Layout";
+import ProductList from "./pages/ProductList";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<ProductList />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
